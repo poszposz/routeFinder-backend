b8532e8112342709895f0ae7ed75d2b854c56105
poszposz <jan.posz@netguru.pl>
2019-08-02 21:23:53 +0200

    fixed graph creation algorithm

--- backend/optimization/graphCreator.js
+++ backend/optimization/graphCreator.js
@@ -1,5 +1,6 @@
 const distanceCalculation = require('./../utilities/distanceCalculation');
 const Vertex = require('./vertex');
+const Graph = require('./graph');
 
 const desiredDistanceThreshold = 20;
 
@@ -8,11 +9,11 @@ function createGraph(routes) {
   let vertices = [];
   let id = 1;
   routes.forEach((route) => {
-    console.log('Going through route');
+    console.log('Going through route');    
     let removeableVertexIds = [];
-    let extractedIncomingNeighbors = [];
-    let extractedOutcomingNeighbors = [];
-    const outcomingNeighbors = routes.filter((filteredRoute) => {
+    let extractedIncomingRoutes = [];
+    let extractedOutcomingRoutes = [];
+    let outcomingNeighbors = routes.filter((filteredRoute) => {
       const distance = distanceCalculation.distanceBetweenLocations(route.start, filteredRoute.start);
       const eligible =  distance < desiredDistanceThreshold;
       if (eligible === true) {
@@ -20,23 +21,19 @@ function createGraph(routes) {
           // Means that found start is already a part of some vertex
           // We have to take start point vertex id and add it to removeable ids
           // We have to add all incoming and outcoming routes to the newly created vertex.
-          console.log(`Vertices: ${vertices.map((vertex) => vertex.id)}`);
-          console.log(`Id of already existing vertex: ${filteredRoute.startPointVertexId}`);
           let vertex = vertices.find((iteratedVertex) => {
             return iteratedVertex.id === filteredRoute.startPointVertexId
           });
-          console.log(`Found vertex: ${vertex.outcomingNeighbors}`);
-          extractedIncomingNeighbors.concat(vertex.id);
-          extractedOutcomingNeighbors.concat(vertex.outcomingNeighbors);
+          extractedIncomingRoutes = extractedIncomingRoutes.concat(vertex.incomingRoutes);
+          extractedOutcomingRoutes = extractedOutcomingRoutes.concat(vertex.outcomingRoutes);
           removeableVertexIds.push(filteredRoute.startPointVertexId);
         } else {
-          console.log('Found outcoming neighbor');
           filteredRoute.markedStart = true;
         }
       }
       return eligible;
     });
-    const incomingNeighbors = routes.filter((filteredRoute) => {
+    let incomingNeighbors = routes.filter((filteredRoute) => {
       const distance = distanceCalculation.distanceBetweenLocations(route.start, filteredRoute.end);
       const eligible = distance < desiredDistanceThreshold;
       if (eligible === true) {
@@ -45,31 +42,31 @@ function createGraph(routes) {
           // We have to take end point vertex id and add it to remove'able ids
           // We have to add all incoming and outcoming routes to the newly created vertex.
           let vertex = vertices.find(iteratedVertex => iteratedVertex.id === filteredRoute.endPointVertexId);
-          extractedIncomingNeighbors.concat(vertex.incomingNeighbors);
-          extractedOutcomingNeighbors.concat(vertex.outcomingNeighbors);
+          extractedIncomingRoutes = extractedIncomingRoutes.concat(vertex.incomingRoutes);
+          extractedOutcomingRoutes = extractedOutcomingRoutes.concat(vertex.outcomingRoutes);
           removeableVertexIds.push(filteredRoute.endPointVertexId);
         } else {
-          console.log('Found incoming neighbor');
           filteredRoute.markedEnd = true;
         }
       }
       return eligible;
     });
-    incomingNeighbors.concat(extractedIncomingNeighbors);
-    outcomingNeighbors.concat(extractedOutcomingNeighbors);
+    incomingNeighbors = incomingNeighbors.concat(extractedIncomingRoutes);
+    outcomingNeighbors = outcomingNeighbors.concat(extractedOutcomingRoutes);
+    incomingNeighbors.forEach((route) => {
+      route.endPointVertexId = id;
+    });
+    outcomingNeighbors.forEach((route) => {
+      route.startPointVertexId = id;
+    });
     const vertex = new Vertex(id, incomingNeighbors, outcomingNeighbors);
     id = id + 1;
     vertices.push(vertex);
-    incomingNeighbors.forEach((route) => {
-      route.endPointVertexId = vertex.id;
-    });
-    outcomingNeighbors.forEach((route) => {
-      route.startPointVertexId = vertex.id;
-    });
     vertices = vertices.filter((vertex) => {
       return !removeableVertexIds.includes(vertex.id);
     });
-  }); 
+  });
+  return vertices;
 };
 
 module.exports = createGraph;
--- backend/optimization/vertex.js
+++ backend/optimization/vertex.js
@@ -2,8 +2,8 @@ class Vertex {
 
   constructor(id, incomingRoutes, outcomingRoutes) {
     this.id = id;
-    this.incomingRoutes = incomingRoutes;
-    this.outcomingRoutes = outcomingRoutes;
+    this.incomingRoutes = incomingRoutes === undefined ? [] : incomingRoutes;
+    this.outcomingRoutes = outcomingRoutes === undefined ? [] : outcomingRoutes;
   }
 }
 
